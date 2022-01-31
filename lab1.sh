1a2b5c8636728102f01f7a01a93bef8b1c6d0c46
Khaldun Askara <mkurotsuchi0@gmail.com>
2022-01-31 21:10:07 +0000

    sed done

--- lab1.sh
+++ lab1.sh
@@ -13,3 +13,19 @@ grep -cE '^[АЕЁИОУЫЭЮЯаеёиоуыэюя]{3,}' names.txt
 echo ""
 echo "4 Последняя фамилия по алфавиту:"
 sort -r names.txt | grep -m1 ""
+echo ""
+echo "sed"
+echo "Файл с улицами: "
+cat streets.txt
+echo ""
+echo "1 улицы с 3 гласных"
+sed -n -r '/^[АЕЁИОУЫЭЮЯаеёиоуыэюя]{3,}/p'  streets.txt
+echo ""
+echo "2 ская на ***"
+sed 's/ская/***/g' streets.txt
+echo ""
+echo "3 ^^^ после ская"
+sed '/ская/a^^^' streets.txt
+echo ""
+echo "4 абв в бва"
+sed 'y/абвгдеёжзийклмнопрстуфхцчшщъыьэюя/бвгдеёжзийклмнопрстуфхцчшщъыьэюяа/' streets.txt
